b46baf6f4ae0c54b2f6cc266e3a487e55c3f1006
OmarHamdann <omar.haamdan@gmail.com>
2022-07-13 13:33:17 +0300

    add new page and run app

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -23,6 +23,20 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+
+        public IActionResult Test()
+        {
+            Product rec = new Product
+            {
+                Id = 1,
+                ProductName="car",
+                OrderId=1,
+
+            };
+            ViewData["data"] = rec;
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
